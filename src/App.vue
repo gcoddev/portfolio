9d45b6076562802be1095d3a5373989810c1a3b6
garycdev <admin@garycdev.com>
2024-12-29 23:10:26 -0400

    update 2

--- src/App.vue
+++ src/App.vue
@@ -24,39 +24,6 @@ export default {
         );
     }
     this.getStatus();
-    this.axios
-      .get(
-        "https://api.ipgeolocation.io/ipgeo?apiKey=" +
-        process.env.VUE_APP_API_GEO_KEY
-      )
-      .then((response) => {
-        // console.log(response);
-
-        const region = response.data.country_code2;
-        if (
-          region === "MX" ||
-          region === "AR" ||
-          region === "CO" ||
-          region === "CL" ||
-          region === "PE" ||
-          region === "VE" ||
-          region === "UY" ||
-          region === "PY" ||
-          region === "BO" ||
-          region === "EC"
-        ) {
-          document.getElementsByTagName("html")[0].setAttribute("lang", "es");
-          this.$router.push("/es");
-        } else {
-          document.getElementsByTagName("html")[0].setAttribute("lang", "en");
-          this.$router.push("/en");
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching geolocation data:", error);
-        document.getElementsByTagName("html")[0].setAttribute("lang", "en");
-        this.$router.push("/en");
-      });
   },
 };
 </script>
